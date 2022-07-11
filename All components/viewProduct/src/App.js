6ede45a8493b5faa432e073ac1165f6b95704036
Suvidnya <78336837+Suvidnya@users.noreply.github.com>
2022-07-11 18:20:46 +0530

    Add files via upload

---
+++ All components/viewProduct/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./App.css";
+import { Firstrow } from './components/firstrow';
+import { Navbar } from './components/navbar';
+import { Secondrow } from './components/secondrow';
+import { Footer } from './components/footer';
+
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Firstrow/>
+    <Secondrow/><br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <Footer/>
+    
+    </>
+  );
+}
+
+export default App;
---
+++ All components/viewProduct/src/components/footer.js
@@ -0,0 +1,126 @@
+import React from 'react';
+import './footer.css'
+
+export const Footer = () => {
+  return (
+    <div>
+
+
+<footer className="page-footer font-small blue pt-4">
+
+  
+  <div className="container-fluid text-center text-md-left">
+
+    
+    <div className="row">
+
+      
+      <div className="col-md-3 mt-md-0 mt-3">
+
+        
+        <h4 className="text-uppercase textwhite">OlxClone by NBSS</h4>
+        <p className='textwhite para'>ohho bhai ye to abb itna hi bana itna banate banate hi toh g...d fat gyi jo log puri website banate honge code krke usnki to khatam bhai. to aaisi baat hai bhai wo</p>
+
+      </div>
+      
+
+      <hr className="clearfix w-100 d-md-none pb-3"/>
+
+      
+      <div className="col-md-3 mb-md-0 mb-3">
+
+        
+        <h5 className="text-uppercase textwhite">Browse Categories</h5>
+
+        <ul className="list-unstyled">
+          <li>
+            <a href="#!">Electronics</a>
+          </li>
+          <li>
+            <a href="#!">Books</a>
+          </li>
+          <li>
+            <a href="#!">Daily products</a>
+          </li>
+          <li>
+            <a href="#!">Link 4</a>
+          </li>
+        </ul>
+
+      </div>
+
+      <div className="col-md-3 mb-md-0 mb-3">
+
+        
+        <h5 className="text-uppercase textwhite">Contact Us</h5>
+
+        <ul className="list-unstyled">
+          <li>
+            <a href="#!">Link 1</a>
+          </li>
+          <li>
+            <a href="#!">Link 2</a>
+          </li>
+          <li>
+            <a href="#!">Link 3</a>
+          </li>
+          <li>
+            <a href="#!">Link 4</a>
+          </li>
+        </ul>
+
+      </div>
+      
+
+      
+      <div className="col-md-3 mb-md-0 mb-3">
+
+        
+        <h5 className="text-uppercase textwhite">About Us</h5>
+
+        <ul className="list-unstyled">
+          <li>
+            <a href="#!">Link 1</a>
+          </li>
+          <li>
+            <a href="#!">Link 2</a>
+          </li>
+          <li>
+            <a href="#!">Link 3</a>
+          </li>
+          <li>
+            <a href="#!">Link 4</a>
+          </li>
+        </ul>
+
+      </div>
+      
+
+    </div>
+    
+
+  </div>
+  
+
+  
+  <div className="footer-copyright text-center textwhite py-3">© 2022 Copyright : 
+    <a href="/"> Suvidnya,</a>
+    <a href="/"> Nirupma,</a>
+    <a href="/"> Bijir,</a>
+    <a href="/"> Shashank</a>
+  </div>
+  
+
+</footer>
+
+    </div>
+  )
+}
+
+
+
+
+
+
+
+
