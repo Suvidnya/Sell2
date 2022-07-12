6099d16ba072f78c23bd8cfda5a357371c6ed67a
Bijirgiri Basumatary <75986148+bijirgiriiitg@users.noreply.github.com>
2022-07-12 17:42:27 +0530

    Update Ads.js

--- Final/Frontend/src/components/dashboard/home/ads/Ads.js
+++ Final/Frontend/src/components/dashboard/home/ads/Ads.js
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import { useNavigate } from 'react-router-dom'
 import './Ads.css'
 
 const Ads = (props) => {
+  const navigate= useNavigate();
   return (
     <>
         <div className='ads'>
@@ -10,7 +11,7 @@ const Ads = (props) => {
         </div>
         <div className='sellBuyBtn'>
             <button>Sell Products</button>
-            <button>Buy Products</button>
+            <button  onClick={()=>navigate("/view")}>Buy Products</button>
         </div>
     </>
   )
